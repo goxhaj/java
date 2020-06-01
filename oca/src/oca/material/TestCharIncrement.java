50abb681dabc5cc1d4103e8492bef907282fe28a
Ardi Goxhaj <ardi.goxhaj@ikubinfo.al>
2020-06-01 11:01:24 +0200

    clean code

---
+++ oca/src/oca/material/TestCharIncrement.java
@@ -0,0 +1,10 @@
+package oca.material;
+
+public class TestCharIncrement {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    char a = 'A';
+    System.out.println((a + 1));
+  }
+}
